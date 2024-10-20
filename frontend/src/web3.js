dd93bb65b8edc422d4fa74e6e0266a002b86b842
polimerniytoken <polimerniytoken@gmail.com>
2024-10-20 17:50:15 +0300

    Create web3.js

---
+++ frontend/src/web3.js
@@ -0,0 +1,21 @@
+// frontend/src/web3.js
+import Web3 from 'web3';
+
+const getWeb3 = () =>
+  new Promise((resolve, reject) => {
+    window.addEventListener('load', async () => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      } else {
+        reject(new Error('Install MetaMask to use this dApp.'));
+      }
+    });
+  });
+
+export default getWeb3;
